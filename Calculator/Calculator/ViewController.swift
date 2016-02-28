8eda0a9e55074bd8dd4ad9e0ca5b6ec24c80d738
iqbalyarkhan <iykhan@smu.edu>
2016-02-27 23:46:39 -0600

    Commits initial files necessary for the app

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Iqbal Khan on 2/27/16.
+//  Copyright © 2016 Iqbal Yar Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+ 
+    @IBOutlet weak var display: UILabel!
+    
+    //func for function declaration,
+    //appendDigit is the name of the function,
+    //sender is the name of the first argument 
+    //and UIButton is the type of argument.
+    //A return type would be specified by:
+    // after UIBotton) -> Double
+    @IBAction func appendDigit(sender: UIButton)
+    {
+        
+    }
+}
+
